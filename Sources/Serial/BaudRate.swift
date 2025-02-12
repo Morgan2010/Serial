222e9e60d57bb8c685447dfc7a50f9d8ec5803f7
Morgan McColl <morgan.mccoll92@gmail.com>
2025-02-13 04:12:42 +1000

    Linted files.

--- Sources/Serial/BaudRate.swift
+++ Sources/Serial/BaudRate.swift
@@ -1,30 +1,30 @@
 // BaudRate.swift
 // Serial
-// 
+//
 // Created by Morgan McColl.
 // Copyright © 2025 Morgan McColl. All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above
 //    copyright notice, this list of conditions and the following
 //    disclaimer in the documentation and/or other materials
 //    provided with the distribution.
-// 
+//
 // 3. All advertising materials mentioning features or use of this
 //    software must display the following acknowledgement:
-// 
+//
 //    This product includes software developed by Morgan McColl.
-// 
+//
 // 4. Neither the name of the author nor the names of contributors
 //    may be used to endorse or promote products derived from this
 //    software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -36,18 +36,18 @@
 // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // -----------------------------------------------------------------------
 // This program is free software; you can redistribute it and/or
 // modify it under the above terms or under the terms of the GNU
 // General Public License as published by the Free Software Foundation;
 // either version 2 of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, see http://www.gnu.org/licenses/
 // or write to the Free Software Foundation, Inc., 51 Franklin Street,
@@ -56,7 +56,7 @@
 import CSerial
 
 /// A type representing baud rates of a serial port.
-/// 
+///
 /// The enum represents both predefined baud rates and custom baud rates.
 public enum BaudRate: CTypeConvertible, Equatable, Hashable, Codable, Sendable, ExpressibleByIntegerLiteral {
 
@@ -71,11 +71,13 @@ public enum BaudRate: CTypeConvertible, Equatable, Hashable, Codable, Sendable, 
         switch self {
         case .custom(let value):
             return CSERIAL_BAUDRATE_TYPE(
-                baud_rate: CSERIAL_BaudRate_Union(baud_rate: value), is_predefined: false
+                baud_rate: CSERIAL_BaudRate_Union(baud_rate: value),
+                is_predefined: false
             )
         case .predefined(let value):
             return CSERIAL_BAUDRATE_TYPE(
-                baud_rate: CSERIAL_BaudRate_Union(predefined: value.ctype), is_predefined: true
+                baud_rate: CSERIAL_BaudRate_Union(predefined: value.ctype),
+                is_predefined: true
             )
         }
     }
@@ -92,7 +94,7 @@ public enum BaudRate: CTypeConvertible, Equatable, Hashable, Codable, Sendable, 
     }
 
     /// Create a `BaudRate` from an integer literal.
-    /// 
+    ///
     /// If the integer falls within the predefined range (see ``PredefinedBaudRate``) then the baud rate
     /// will be an instance of the `predefined` case. Otherwise it will be an instance of the `custom` case.
     /// - Parameter value: The baud rate of the serial port.
--- Sources/Serial/PredefinedBaudRate.swift
+++ Sources/Serial/PredefinedBaudRate.swift
@@ -1,30 +1,30 @@
 // PredefinedBaudRate.swift
 // Serial
-// 
+//
 // Created by Morgan McColl.
 // Copyright © 2025 Morgan McColl. All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
-// 
+//
 // 1. Redistributions of source code must retain the above copyright
 //    notice, this list of conditions and the following disclaimer.
-// 
+//
 // 2. Redistributions in binary form must reproduce the above
 //    copyright notice, this list of conditions and the following
 //    disclaimer in the documentation and/or other materials
 //    provided with the distribution.
-// 
+//
 // 3. All advertising materials mentioning features or use of this
 //    software must display the following acknowledgement:
-// 
+//
 //    This product includes software developed by Morgan McColl.
-// 
+//
 // 4. Neither the name of the author nor the names of contributors
 //    may be used to endorse or promote products derived from this
 //    software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -36,18 +36,18 @@
 // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 // -----------------------------------------------------------------------
 // This program is free software; you can redistribute it and/or
 // modify it under the above terms or under the terms of the GNU
 // General Public License as published by the Free Software Foundation;
 // either version 2 of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, see http://www.gnu.org/licenses/
 // or write to the Free Software Foundation, Inc., 51 Franklin Street,
@@ -173,7 +173,7 @@ public enum PredefinedBaudRate: CTypeConvertible, Equatable, Hashable, Codable, 
     }
 
     /// Initialise the PredefinedBaudRate from an integer baud rate.
-    /// 
+    ///
     /// If the baud rate is not within the predefined values, then a value of `nil` is returned.
     /// - Parameter baudRate: The baud rate to initialise from.
     @inlinable
